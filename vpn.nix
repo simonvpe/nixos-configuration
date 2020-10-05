0b773e2c68e95430cf30a94c7b670080cdecb0b6
Simon Pettersson <simon.pettersson@rco.se>
2020-10-05 11:15:18 +0200

    Uses bitwarden for vpn password

--- vpn.nix
+++ vpn.nix
@@ -2,9 +2,14 @@
 , writeScriptBin
 , vpnHost ? "193.13.80.146:443"
 , vpnUser ? "simonp"
+, bwObject ? "18da6c75-397a-4070-ae21-ac4b009365a5"
+, nodePackages
 }:
 
 writeScriptBin "vpn" ''
-  sudo ${openfortivpn}/bin/openfortivpn ${vpnHost} -u ${vpnUser} \
+  sudo ${openfortivpn}/bin/openfortivpn \
+    ${vpnHost} \
+    -u ${vpnUser} \
+    -p $(${nodePackages.bitwarden-cli}/bin/bw --raw get password ${bwObject}) \
     --trusted-cert fde5cf653d6714ee7816c6cd77f4645697771c48f7e01a3a090e1f13a70b7816
 ''
